e1f73e380013d1dd4c81a45838da4d7060120cad
Alena <attapi343@gmail.ru>
2020-12-31 17:45:25 +0300

    Refactored map component structure.

---
+++ src/app/map/map-browser/map-browser.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+
+@Component({
+  selector: 'app-map-browser',
+  templateUrl: './map-browser.component.html',
+  styleUrls: ['./map-browser.component.scss']
+})
+export class MapBrowserComponent implements OnInit {
+
+  public map?: Map;
+
+  ngOnInit(): void {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([7.0785, 51.4614]),
+        zoom: 5
+      })
+    });
+  }
+}
--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { MapComponent } from './map.component';
-import { registerLocaleData } from '@angular/common';
+import {MapComponent} from './map.component';
+import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
+import {MapBrowserComponent} from './map-browser/map-browser.component';
 
 registerLocaleData(en);
 
 @NgModule({
   imports: [],
-  declarations: [MapComponent],
+  declarations: [MapComponent, MapBrowserComponent],
   exports: [MapComponent]
 })
-export class MapModule { }
+export class MapModule {
+}
