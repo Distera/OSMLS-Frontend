574b20690e74cf6733c135f893b41148a0e147f5
Alena Burova <attapi343@gmail.com>
2021-08-13 16:47:29 +0300

    Implemented event model and infrastructure.

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -3,12 +3,24 @@ import {NgModule} from '@angular/core';
 import {MapComponent} from './map.component';
 import {CommonModule} from '@angular/common';
 import {MapBrowserComponent} from './map-browser/map-browser.component';
+import {MapPropertiesComponent} from './map-properties/map-properties.component';
+import {NzTableModule} from 'ng-zorro-antd/table';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {NzGridModule} from 'ng-zorro-antd/grid';
+import {NzCollapseModule} from 'ng-zorro-antd/collapse';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {VarDirective} from 'src/app/infrastructure/ng-var.directive';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NzTableModule,
+    NzButtonModule,
+    NzGridModule,
+    NzCollapseModule,
+    NzInputModule,
   ],
-  declarations: [MapComponent, MapBrowserComponent],
+  declarations: [MapComponent, MapBrowserComponent, MapPropertiesComponent, VarDirective],
   exports: [MapComponent]
 })
 export class MapModule {
